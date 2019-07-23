ea2f0365e4f5c55af376cd643c7ed850b534c514
Kaur4058 <52577631+Kaur4058@users.noreply.github.com>
2019-07-23 16:10:43 -0400

    Add files via upload

--- assets/data/mySongs.ts
+++ assets/data/mySongs.ts
@@ -1,7 +1,7 @@
 import {SongInfo} from '../../app/songInfo';
 
 export const  MYSONGS: SongInfo[]= [
-    {sname:'Baby',artist:'Justin',genre:'Pop',yreleased:2010,picture:'Kaur4058A3/assets/images/baby.jpg'},
+    {sname:'Baby',artist:'Justin',genre:'Pop',yreleased:2010,picture:'assets/images/baby.jpg'},
     {sname:'Sorry',artist:'Justin',genre:'Dancehall-pop',yreleased:2015,picture:'assets/images/sorry.png'},
     {sname:'Cheapthrills',artist:'Siya furler',genre:'Dancehall',yreleased:2016,picture:'assets/images/cheapthrills.jpg'},
     {sname:'Dont let me down',artist:'Chainsmokers',genre:'EDM',yreleased:2016,picture:'assets/images/chainsmokers.jpg'} 
