58b205ad84a1853b0a7c0c21d7e05ddcaea9491b
Kaur4058 <52577631+Kaur4058@users.noreply.github.com>
2019-07-23 15:10:59 -0400

    uploaded build folder

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,8 @@
+import {SongInfo} from '../../app/songInfo';
+
+export const  MYSONGS: SongInfo[]= [
+    {sname:'Baby',artist:'Justin',genre:'Pop',yreleased:2010,picture:'../../assets/images/baby.jpg'},
+    {sname:'Sorry',artist:'Justin',genre:'Dancehall-pop',yreleased:2015,picture:'../../assets/images/sorry.png'},
+    {sname:'Cheapthrills',artist:'Siya furler',genre:'Dancehall',yreleased:2016,picture:'../../assets/images/cheapthrills.jpg'},
+    {sname:'Dont let me down',artist:'Chainsmokers',genre:'EDM',yreleased:2016,picture:'../../assets/images/chainsmokers.jpg'} 
+];
